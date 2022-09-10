f403daa8ca190533afc4056553aca5a7999cbc7e
Kobe McManus <kobe.mcmanus@xenixi.com>
2022-09-10 17:52:02 -0400

    initial

---
+++ src/com/xenixi/examples/one/main/HelloWorld.java
@@ -0,0 +1,14 @@
+/* Basic 'Hello World' */
+package com.xenixi.examples.one.main;
+
+// Class declaration
+public class HelloWorld {
+
+	// Java main method
+	public static void main(String[] args) {
+
+		// Print to console
+		System.out.println("Hello World! :O");
+
+	}
+}
---
+++ src/com/xenixi/examples/one/main/Types.java
@@ -0,0 +1,32 @@
+package com.xenixi.examples.one.main;
+
+public class Types {
+
+	/* Java Primitive Types */
+	
+	// 8 bit signed integer
+	byte exampleByte = -128; // MIN_VALUE
+	byte exampleByte2 = 127; // MAX_VALUE
+
+	// 16 bit signed integer
+	short exampleShort = -32768; // MIN_VALUE
+	short exampleShort2 = 32767; // MAX_VALUE
+
+	// 32 bit signed integer
+	int exampleInt = -2147483648; // MIN_VALUE
+	int exampleInt2 = 2147483647; // MAX_VALUE
+
+	// 64 bit signed integer
+	long exampleLong = -9223372036854775808L; // MIN_VALUE
+	long exampleLong2 = 9223372036854775807L; // MAX_VALUE
+
+	// 32 bit decimal
+	float exampleFloat = 5.09f;
+
+	// 64 bit decimal
+	double exampleDouble = 5.9348670795;
+
+	// 16 bit character (Unicode)
+	char exampleChar = 'c';
+
+}
